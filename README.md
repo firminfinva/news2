a61597b7df86de8eeb20274e8bc23b42463ce58b
firmin nagnduli <finganduli@gmail.com>
2023-09-29 09:20:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# mixed_messages_kadea
+
+create-sets-random-words // this is a branch
+
+
+the program generate a sentence from 3 sets of randoms words
+the program gives the option to stop the program by entering the number less than 2 
+if the number is greater than one the program execute again
+
+
+the username julmug is actually landryk69
+
+
+
+
+NB: the nain branch was created after and the old main is now develop
---
+++ index.js
@@ -0,0 +1,22 @@
+// ocde here
+let set1 = ['i', 'you', 'he or she', 'we', 'they' ]
+
+let set2 = ['Accept', 'fuck', 'suck', 'kill', 'kiss', 'eat','get', 'touch', 'marry']
+
+let set3 = ['goat', 'cow', 'a girl', 'old woman', 'meat', 'chips', 'snake']
+
+// create sentence with this  sets
+let ans1 = ""
+let ans2 = ""
+let ans3 = ""
+let ans = 9
+while(ans>2){
+    const randomIndex = Math.floor(Math.random() * set1.length);
+    const ans1 = set1[randomIndex];
+     const randomIndex1 = Math.floor(Math.random() * set2.length);
+    const ans2= set2[randomIndex1];
+    const randomIndex2 = Math.floor(Math.random() * set3.length);
+    const ans3= set3[randomIndex2];
+  console.log(ans1,ans2,ans3);  
+  ans  = parseInt(prompt("continue playing again"));
+}
